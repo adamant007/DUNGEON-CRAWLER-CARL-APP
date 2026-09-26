ac6eb5111e3a24c1a4cad35c9168c453e89ac99e
adamant007 <71805161+adamant007@users.noreply.github.com>
2026-09-26 10:32:28 -0700

    Simplify whole-term profanity matcher

--- src/lib/textSafety.js
+++ src/lib/textSafety.js
@@ -70,7 +70,7 @@ const escaped = (ch) => ch.replace(/[.*+?^$(){}|[\]\\]/g, "\\$&");
 
 const obfuscatedTermRegex = (term) => {
   const letters = normalizeSafetyText(term).replace(/[^a-z0-9]/g, "").split("");
-  const body = letters.map((ch) => escaped(ch) + "+[^a-z0-9]*").join("").replace(/\\[\\^a-z0-9\\]\\*$/, "");
+  const body = letters.map((ch) => escaped(ch) + "+[^a-z0-9]*").join("");
   return new RegExp("(^|[^a-z0-9])" + body + "($|[^a-z0-9])", "i");
 };
 
