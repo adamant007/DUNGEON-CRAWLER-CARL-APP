829bbccfcacf1e1a5e93a535fe784f3e54ce9e2e
adamant007 <71805161+adamant007@users.noreply.github.com>
2026-09-25 18:27:44 -0700

    Remove legacy bottom dock globally

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import Campaign from './pages/Campaign';
 import GmTools from './pages/GmTools';
 import ClaimCharacter from './pages/ClaimCharacter';
 import NavLayout from './components/NavLayout';
+import LegacyDockCleanup from './components/LegacyDockCleanup';
 
 const AuthenticatedApp = () => {
   const { isLoadingAuth, isLoadingPublicSettings, authError, navigateToLogin } = useAuth();
@@ -49,6 +50,7 @@ const AuthenticatedApp = () => {
   // Render the main app
   return (
     <>
+    <LegacyDockCleanup />
     <Routes>
     {/* Add your page Route elements here */}
     <Route path="/" element={<Home />} />
